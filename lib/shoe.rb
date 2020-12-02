648bd83cd1d580cd380a686e3251ea256774eef2
Michael Fuget <michaelfuget@gmail.com>
2020-12-02 07:12:45 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,21 @@
 # Make your shoe class here!
+class Shoe
+  attr_accessor :author
+  attr_accessor :page_count
+  attr_accessor :genre
+  attr_reader :title
+  
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def title
+    @title 
+  end
+  
+  def cobble
+    @condition = "new"
+    puts 
+  end 
+  
+end
